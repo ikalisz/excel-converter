1076646c0f51444d5d56d3a90a2634cd72ad5d92
ikalisz <ikalisz@yahoo.com>
2024-04-02 14:35:55 -0500

    adding packages and testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import get from 'lodash/get';
 
 function App() {
+  const [file, setFile] = useState({})
+
+  const fileTypes = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+  const fileOnChange = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.files)
+    const file = e.target.files[0];
+    setFile(e.target.files[0])
+    console.log(file)
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (!event?.target?.result) {
+        return;
+      }
+       const {result} = event.target;
+       
+    };
+    const test = reader.readAsText(file);
+    console.log('test', test);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <label>Selected File Name: {get(file, 'name', '')}</label>
+        <Button
+          component="label"
+          variant="outlined"
+          startIcon={<UploadFileIcon />}
+          sx={{ marginRight: "1rem" }}
         >
-          Learn React
-        </a>
+          Upload CSV
+          <input type="file" accept={fileTypes} hidden onChange={fileOnChange} />
+        </Button>
+        {/* <Button onClick={fileOnChange} >
+          <input type="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" hidden/>
+        </Button>
+        <TextField type="file" id="fileinput" onChange={fileOnChange} inputProps={{accept:fileTypes}}/> */}
       </header>
     </div>
   );
